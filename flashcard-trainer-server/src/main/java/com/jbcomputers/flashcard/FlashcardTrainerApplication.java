eab209e8fc332ccd0184658353243baac67337ca
Jason Bandy <jbandy98@gmail.com>
2024-02-26 21:54:55 -0600

    Initial commit of base projects

---
+++ flashcard-trainer-server/src/main/java/com/jbcomputers/flashcard/FlashcardTrainerApplication.java
@@ -0,0 +1,13 @@
+package com.jbcomputers.flashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardTrainerApplication.class, args);
+	}
+
+}
---
+++ flashcard-trainer-server/src/test/java/com/jbcomputers/flashcard/FlashcardTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbcomputers.flashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardTrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
